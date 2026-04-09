d94de03a96f9767c13be98d5a080b45cf4c36fa6
MuchiraIrungu <cmuchirairungu@gmail.com>
2026-04-09 22:29:37 +0300

    fix login issue

--- frontend/app/components/SidebarComponent.tsx
+++ frontend/app/components/SidebarComponent.tsx
@@ -139,9 +139,10 @@ export default function Sidebar() {
 
             if (res.status === 200) {
                 localStorage.removeItem("role");
+                sessionStorage.clear();
+                document.cookie = "user_role=; path=/; max-age=0; SameSite=Lax";
                 // Small delay so the animation feels intentional
                 await new Promise((resolve) => setTimeout(resolve, 800));
-                document.cookie = "user_role=; path=/; max-age=0; SameSite=Lax";
                 
                 router.push("/auth/login");
             } else {
