afd1d1df0acbb1976e78c11c584e2f74c4e4f188
MuchiraIrungu <cmuchirairungu@gmail.com>
2026-04-09 20:28:05 +0300

    students issue changes and session issue fix

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -17,6 +17,10 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
+from dotenv import load_dotenv 
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
